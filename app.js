26002e179f9f8a05a6b20d3760cbb47629ddf7c0
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-05-01 15:19:59 -0300

    default itens

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
-//jshint esversion:6
+const express = require ("express"); 
+const bodyParser = require ("body-parser"); 
+const mongoose = require ("mongoose"); 
+ 
+const app = express (); 
+ 
+app.set ("view engine", "ejs"); 
+ 
+app.use(bodyParser.urlencoded({extended: true})); 
+app.use(express.static("public")); 
+ 
+mongoose.connect("mongodb://0.0.0.0:27017/todoListDB" , {useNewUrlParser: true});
+ 
+const itemsSchema = {
+  name: String
+}; 
+ 
+const Item = mongoose.model("item", itemsSchema); 
+ 
+const item1 = new Item ({
+  name: "Welcome to your to do list!"
+}); 
+ 
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+}); 
+ 
+const item3 = new Item ({
+  name: "<- Hit this to delete an item."  
+}); 
+ 
+const defaultItems = [item1, item2, item3]; 
+ 
+app.get("/", function (req,res) {
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
 
-const app = express();
+  //printing all store values in terminal (In my case Hyper Terminal)
+  Item.find({})
+    .then(foundItem => {
+      if (foundItem.length === 0) {
+        return Item.insertMany(defaultItems);
+      } else {
+        return foundItem;
+      }
+    })
+    .then(savedItem => {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: savedItem
+      });
+    })
+    .catch(err => console.log(err));
 
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("public"));
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
-
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+ 
+  //res.render("list", {listTitle:"Today", newListItems:defaultItems});
+ 
 });
-
-app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
+ 
+app.post("/", function (req,res){
+ 
+  const item = req.body.newItem; 
+ 
   if (req.body.list === "Work") {
     workItems.push(item);
-    res.redirect("/work");
-  } else {
+    res.redirect("/work"); 
+  }else{
     items.push(item);
     res.redirect("/");
   }
 });
-
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+ 
+app.get("/about", function(){
+  res.render("about"); 
 });
-
-app.get("/about", function(req, res){
-  res.render("about");
-});
-
-app.listen(3000, function() {
+ 
+app.listen(3000, function (){
   console.log("Server started on port 3000");
 });
